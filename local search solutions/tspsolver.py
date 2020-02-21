ce1cfe108eef831944fa8fe14b68e10ba4640d66
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-02-21 13:53:45 +0200

    Update tspsolver.py

--- local search solutions/tspsolver.py
+++ local search solutions/tspsolver.py
@@ -1,45 +1,42 @@
-from tkinter import *
-from tkinter import messagebox as msg
-from tkinter import filedialog
+from tkinter import Menu, Button, StringVar, OptionMenu, messagebox as msg, filedialog, Tk
 from nearestneighbor import *
 import pandas as pd 
 import numpy as np
 class TSP_SOLVER ():
-    
     def __init__(self,master):
         self.master = master
         self.master.title("TSP_SOLVER")
         self.master.geometry("250x120")
-        self.master.resizable(False,False)
+        self.master.resizable(False, False)
         self.filed = ""
 
         #menu
         self.menu = Menu(self.master)
         
-        self.file_menu = Menu(self.menu,tearoff = 0)
-        self.file_menu.add_command(label = "Insert a file",accelerator = 'Ctrl+O',command = self.insertfile)
-        self.file_menu.add_command(label = "Solve",accelerator = 'Alt+F5',command = self.solve)
-        self.file_menu.add_command(label = "Close file",accelerator = "Ctrl+F5",command = self.cf)
-        self.file_menu.add_command(label="Exit",accelerator= 'Alt+F4',command = self.exitmenu)
-        self.menu.add_cascade(label = "File",menu=self.file_menu)
+        self.file_menu = Menu(self.menu, tearoff=0)
+        self.file_menu.add_command(label="Insert a file",accelerator='Ctrl+O', command=self.insertfile)
+        self.file_menu.add_command(label="Solve", accelerator='Alt+F5', command=self.solve)
+        self.file_menu.add_command(label="Close file", accelerator="Ctrl+F5", command=self.cf)
+        self.file_menu.add_command(label="Exit", accelerator='Alt+F4', command=self.exitmenu)
+        self.menu.add_cascade(label="File", menu=self.file_menu)
         
-        self.about_menu = Menu(self.menu,tearoff = 0)
-        self.about_menu.add_command(label = "About",accelerator= 'Ctrl+I',command=self.aboutmenu)
-        self.menu.add_cascade(label="About",menu=self.about_menu)
+        self.about_menu = Menu(self.menu,tearoff=0)
+        self.about_menu.add_command(label="About", accelerator='Ctrl+I', command=self.aboutmenu)
+        self.menu.add_cascade(label="About", menu=self.about_menu)
         
-        self.help_menu = Menu(self.menu,tearoff = 0)
-        self.help_menu.add_command(label = "Help",accelerator = 'Ctrl+F1',command=self.helpmenu)
-        self.menu.add_cascade(label="Help",menu=self.help_menu)
+        self.help_menu = Menu(self.menu,tearoff=0)
+        self.help_menu.add_command(label="Help", accelerator='Ctrl+F1', command=self.helpmenu)
+        self.menu.add_cascade(label="Help", menu=self.help_menu)
         
         self.master.config(menu=self.menu)
-        self.master.bind('<Control-o>',lambda event:self.insertfile())
-        self.master.bind('<Alt-F5>',lambda event:self.solve())
-        self.master.bind('<Alt-F4>',lambda event: self.exitmenu())
-        self.master.bind('<Control-F5>',lambda event: self.cf())
-        self.master.bind('<Control-F1>',lambda event: self.helpmenu())
-        self.master.bind('<Control-i>',lambda event:self.aboutmenu())
+        self.master.bind('<Control-o>', lambda event: self.insertfile())
+        self.master.bind('<Alt-F5>', lambda event: self.solve())
+        self.master.bind('<Alt-F4>', lambda event: self.exitmenu())
+        self.master.bind('<Control-F5>', lambda event: self.cf())
+        self.master.bind('<Control-F1>', lambda event: self.helpmenu())
+        self.master.bind('<Control-i>', lambda event:self.aboutmenu())
         
-        self.binsert = Button(self.master,text = "Insert a file",command = self.insertfile)
+        self.binsert = Button(self.master, text="Insert a file", command=self.insertfile)
         self.binsert.pack()
     
     def cf(self):
@@ -50,57 +47,50 @@ class TSP_SOLVER ():
             self.popupsetmenu.forget()
             self.solvb.forget()
             msg.showinfo("SUCCESS", "FILE CLOSED")
-
-    
     def exitmenu(self):
         if msg.askokcancel("Quit?", "Really quit?"):
             self.master.destroy()
     
-    
-    
     def insertfile(self):
         """ user inserts a .txt file (problem instance ) """
         if self.filed == "":
-            self.filed = filedialog.askopenfilename(initialdir="/",title="Select txt file",
-                                                   filetypes=(("txt files","*.txt"),("all files","*.*")))
+            self.filed = filedialog.askopenfilename(initialdir="/", title="Select txt file",
+                                                   filetypes=(("txt files", "*.txt"), ("all files", "*.*")))
           
             if ".txt" in self.filed:
                 self.table,self.number = fileparser(self.filed)
-                msg.showinfo("SUCCESS" , "THE FILE SUCCESSFULLY INSERTED \nNumber of nodes:" + str(len(self.number)))
+                msg.showinfo("SUCCESS", "THE FILE SUCCESSFULLY INSERTED \nNumber of nodes:" + str(len(self.number)))
                 nodelist = list(self.number)
                 self.varnumnode = StringVar(self.master)
                 self.varnumnode.set(nodelist[0])
-                self.popupsetmenu = OptionMenu(self.master,self.varnumnode,*nodelist)
+                self.popupsetmenu = OptionMenu(self.master, self.varnumnode, *nodelist)
                 self.popupsetmenu.pack()
             
-                self.solvb = Button(self.master,text = "Solve",command = self.solve)
+                self.solvb = Button(self.master, text="Solve", command=self.solve)
                 self.solvb.pack()
             else:
-                msg.showerror("Error","NO TXT FILE ADDED")
+                msg.showerror("Error", "NO TXT FILE ADDED")
         else:
-            msg.showerror("ERROR","YOU NEED TO CLOSE THE FILE")
+            msg.showerror("ERROR", "YOU NEED TO CLOSE THE FILE")
                 
     def helpmenu(self):
-        msg.showinfo("Help","A TSP SOLVER")
+        msg.showinfo("Help", "A TSP SOLVER")
     
     def aboutmenu(self):
-        msg.showinfo("About","Version 1.0")
-        
-
+        msg.showinfo("About", "Version 1.0")
 
     def solve(self):
         """ solves the problem """
         if self.filed  == "":
             msg.showinfo("Import", "You need to import a .txt file")
         else:
-            visited_nodes, totalscore = nearserN(self.table,self.number,self.varnumnode.get()) 
+            visited_nodes, totalscore = nearserN(self.table, self.number, self.varnumnode.get()) 
             msg.showinfo("SUCCESS", "THE ROUTE USING NEAREST NEIGHBOR"+str(visited_nodes)+"with score:"+str(totalscore))
-
-        
     
 def main():
+    """ main function """
     root=Tk()
-    TSP_S = TSP_SOLVER(root)
+    TSP_SOLVER(root)
     root.mainloop()
     
 if __name__=='__main__':
